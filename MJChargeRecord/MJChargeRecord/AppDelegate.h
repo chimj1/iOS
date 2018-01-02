9d82fce40b061289f567ece06f0521d5b40b20fc
chimj1 <442656651@qq.com>
2018-01-02 21:29:44 +0800

    第一版

---
+++ MJChargeRecord/MJChargeRecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJChargeRecord
+//
+//  Created by chimj on 2017/12/21.
+//  Copyright © 2017年 chimj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJChargeRecord/MJChargeRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJChargeRecord
+//
+//  Created by chimj on 2017/12/21.
+//  Copyright © 2017年 chimj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
